2f94f792d3e951ea9134051b0d4968d0f63080a8
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2020-11-17 14:19:35 -0300

    feat: implements To Do List initial structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 function App() {
+  const tasks = [
+    { id: 1, description: 'Comprar pão' },
+    { id: 2, description: 'Lavar louça' },
+    { id: 3, description: 'Comprar ração dos gatos' },
+    { id: 4, description: 'Abastecer carro' },
+  ];
+
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <>
+      <h1>To Do List</h1>
+      <input placeholder="Descrição" />
+      <button>Adicionar</button>
+      <ul>
+        {tasks.map(item => 
+          <li key={item.id}>{item.description}</li>
+        )}
+      </ul>
+    </>
   );
 }
 
