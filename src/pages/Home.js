4d8aa8ab18c267da232867d6f4e23d2c7e1c2631
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2020-11-17 16:07:56 -0300

    feat: implements useEffect to control loading and tasks mock

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import TaskForm from '../components/TaskForm';
 import TaskList from '../components/TaskList';
 
+const TASKS_MOCK = [
+  { id: 1, description: 'Comprar pão' },
+  { id: 2, description: 'Lavar louça' },
+  { id: 3, description: 'Comprar ração dos gatos' },
+  { id: 4, description: 'Abastecer carro' },
+];
+
 const Home = () => {
-  const [tasks, setTasks] = useState([
-    { id: 1, description: 'Comprar pão' },
-    { id: 2, description: 'Lavar louça' },
-    { id: 3, description: 'Comprar ração dos gatos' },
-    { id: 4, description: 'Abastecer carro' },
-  ]);
+  const [loading, setLoading] = useState(false); 
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setTasks(TASKS_MOCK);
+      setLoading(false);
+    }, 2000);
+  }, []);
 
   return (
     <>
       <h1>To Do List</h1>
       <TaskForm />
-      <TaskList tasks={tasks} />
+      {
+        loading ? 
+          <p>Carregando...</p> : 
+          <TaskList tasks={tasks} />
+      }
     </>
   );
 };
